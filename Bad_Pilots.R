b5b663c52876a85e12a5d505ec1c72295f9d681b
Joe Hilgard <jhilgard@gmail.com>
2014-11-29 23:50:05 -0600

    Updated scripts for nicer code. Added code to calculate BF01 for Table 3.

--- Bad_Pilots.R
+++ Bad_Pilots.R
@@ -1,38 +1,38 @@
 # Function for pilot test w/ null-equivalent region.
-equivTestPaired = function(N, t, lo, hi) {
+equivTestPaired = function(N, t, nullInterval=NULL, rscale=.5) {
   x = rnorm(N)
   diff = rnorm(N, sd = sqrt(N))
   diff = diff / sd(diff) * sqrt(N) # rescale 
   diff = diff - mean(diff) + t # recenter
   y = x + diff
-  return(ttestBF(x, y, nullInterval=c(lo, hi), paired=T, rscale=.5))
+  return(ttestBF(x, y, nullInterval=nullInterval, paired=T, rscale=rscale))
 }
-equivTest = function(n1, n2, t, lo, hi) {
+equivTest = function(n1, n2, t, nullInterval=NULL, mu=0, rscale=.5) {
   x = rnorm(n1); x = x - mean(x); x = x / sd(x) # center & scale @ 0
   y = rnorm(n2)
   y = y/sd(y); y = y - mean(y) # center and scale at 0
   SE = sqrt(var(x)/n1 + var(y)/n2)
   y = y + t*SE # center at t*SE
-  return(ttestBF(x, y, nullInterval=c(lo, hi), paired=F, rscale=.5))
+  return(ttestBF(x, y, nullInterval=nullInterval, paired=F, mu=mu, rscale=rscale))
 }
 # Load in BayesFactor package 
 require(BayesFactor)
 
 # Arriaga et al., 2008 and the no good rotten pilot test
-equivTestPaired(20, .48, -.1, .1)
-equivTestPaired(20, .53, -.1, .1)
-equivTestPaired(20, .79, -.1, .1)
-equivTestPaired(20, .83, -.1, .1)
-equivTestPaired(20, .86, -.1, .1)
-equivTestPaired(20, .89, -.1, .1)
-equivTestPaired(20, 1.14, -.1, .1)
-equivTestPaired(20, 1.24, -.1, .1)
-equivTestPaired(20, 1.29, -.1, .1)
-equivTestPaired(20, 1.32, -.1, .1)
-equivTestPaired(20, 1.56, -.1, .1)
-equivTestPaired(20, 1.67, -.1, .1)
-equivTestPaired(20, 2.27, -.1, .1)
-equivTestPaired(20, 2.63, -.1, .1)
+equivTestPaired(20, .48, c(-.1, .1))
+equivTestPaired(20, .53, c(-.1, .1))
+equivTestPaired(20, .79, c(-.1, .1))
+equivTestPaired(20, .83, c(-.1, .1))
+equivTestPaired(20, .86, c(-.1, .1))
+equivTestPaired(20, .89, c(-.1, .1))
+equivTestPaired(20, 1.14, c(-.1, .1))
+equivTestPaired(20, 1.24, c(-.1, .1))
+equivTestPaired(20, 1.29, c(-.1, .1))
+equivTestPaired(20, 1.32, c(-.1, .1))
+equivTestPaired(20, 1.56, c(-.1, .1))
+equivTestPaired(20, 1.67, c(-.1, .1))
+equivTestPaired(20, 2.27, c(-.1, .1))
+equivTestPaired(20, 2.63, c(-.1, .1))
 
 ## Update the section below:
 
@@ -43,30 +43,61 @@ equivTestPaired(20, 2.63, -.1, .1)
 
 # Cell sizes: RDR-v, 15; RDR-nv, 10; FIFA, 15.
 # RDR "violent" vs RDR "nonviolent"
-equivTest(15, 10, 1.82, -.1, .1) # Difficulty
-equivTest(15, 10, 1.31, -.1, .1) # Pace
-equivTest(15, 10, 3.00, -.1, .1) # Competitveness
+equivTest(15, 10, 1.82, c(-.1, .1)) # Difficulty
+equivTest(15, 10, 1.31, c(-.1, .1)) # Pace
+equivTest(15, 10, 3.00, c(-.1, .1)) # Competitveness
 # RDR "violent" vs FIFA
-equivTest(15, 10, 1.47, -.1, .1) # Difficulty
-equivTest(15, 10, 2.00, -.1, .1) # Pace
-equivTest(15, 10, .047, -.1, .1) # Competitveness
+equivTest(15, 10, 1.47, c(-.1, .1)) # Difficulty
+equivTest(15, 10, 2.00, c(-.1, .1)) # Pace
+equivTest(15, 10, .047, c(-.1, .1)) # Competitveness
 # RDR "nonviolent" vs FIFA 
-equivTest(15, 10, 3.45, -.1, .1) # Difficulty
-equivTest(15, 10, 3.43, -.1, .1) # Pace
-equivTest(15, 10, 3.00, -.1, .1) # Competitveness
+equivTest(15, 10, 3.45, c(-.1, .1)) # Difficulty
+equivTest(15, 10, 3.43, c(-.1, .1)) # Pace
+equivTest(15, 10, 3.00, c(-.1, .1)) # Competitveness
 
 # Adachi & Willoughby -- Are they matched?
 # pilot 1, 14 subjects (paired t-test)
-equivTestPaired(14, 4.39, -.1, .1) # Violence?
-equivTestPaired(14, -.46, -.1, .1) # Competition
-equivTestPaired(14, .59, -.1, .1)  # Difficulty
-equivTestPaired(14, .8, -.1, .1)   # Pace
+equivTestPaired(14, 4.39, c(-.1, .1)) # Violence?
+equivTestPaired(14, -.46, c(-.1, .1)) # Competition
+equivTestPaired(14, .59, c(-.1, .1))  # Difficulty
+equivTestPaired(14, .8, c(-.1, .1))   # Pace
 
 # experiment 1, 21 subs in 2 cells each
-equivTest(21, 21, 7.858, -.1, .1) # Violence?
-equivTest(21, 21, -.387, -.1, .1) # Competition
-equivTest(21, 21, -1.59, -.1, .1)  # Difficulty
-equivTest(21, 21, .89, -.1, .1)   # Pace
+equivTest(21, 21, 7.858, c(-.1, .1)) # Violence?
+equivTest(21, 21, -.387, c(-.1, .1)) # Competition
+equivTest(21, 21, -1.59, c(-.1, .1))  # Difficulty
+equivTest(21, 21, .89, c(-.1, .1))   # Pace
+
+# pilot study 2, N = 19 within 
+dat = data.frame("Condition" = rep(c("C_NV", "C_V", "LC_NV", "LC_V"), each=4),
+                 "Violence" = rep(c(0, 1, 0, 1), each=4),
+                 "Compete" = rep(c(1, 1, 0, 0), each=4),
+                 "Outcome" = rep(c("Violence", "Competitiveness", "Difficulty", "Pace"),4),
+                 "Mean" = c(1.52, 5.86, 3.63, 4.74,
+                            5.37, 6.32, 4.47, 5.42,
+                            1, 1.36, 4.68, 4.57,
+                            6.42, 3.18, 4.42, 5.11),
+                 "SD" = c(.7, .76, 1.38, 1.05,
+                          .9, .58, 1.58, .90,
+                          0, .44, 1.16, 1.26,
+                          .84, 1.67, 1.74, 1.29)
+)
+# generate data
+# Shit... this is a huge pain in the ass because it's repeated-measures.
+N = 19
+genData = c()
+for (i in dat$Condition) {
+  for (j in dat$Outcome) {
+    activeRow = dat[dat$Condition == i & dat$Outcome == j,]
+    cellMean = activeRow$Mean
+    cellSD = activeRow$SD
+    cellDat = rnorm(N, cellMean, cellSD)
+    cellDat = cellDat - mean(cellDat) + cellMean # center precisely
+    if (cellSD > 0) cellDat = cellDat / sd(cellDat) # scale precisely
+    newData = data.frame("Condition" = i, "Outcome" = j, "Value" = cellDat)
+    rbind(genData, newData)
+  }
+}
 
 # experiment 2 it would be necessary to have the raw data to perform anovaBF()
 # No, I could simulate this still... I need to pull from a normal,
--- Bayes_factors_for_MS2.R
+++ Bayes_factors_for_MS2.R
@@ -2,35 +2,81 @@ source("~/R/bayesFactorCalc2.R") # load Christie, Kaye, & Baguley's version of D
 
 #Bf(sd, obtained, uniform = FALSE, lower=0, upper=1, meanoftheory=0, sdtheory=1, tails=2)
 
+# could also use my equivTest function but that requires the exact per-cell N
+# List of BF01's from JZS Bayes, scale=.4
+esciTest = function(obtained, n, rscale, paired=F) {
+  se = sqrt((1-obtained^2)/(n-2))
+  t = obtained / se
+  if (paired==F) return(equivTest(ceiling(n/2), floor(n/2), t=t, rscale=rscale))
+  else return(equivTestPaired(n, t, rscale=rscale))
+} 
 # Aggressive affect
-# Valdez & Ferguson, interaction effect violent (RDR either version) vs nonviolent (FIFA)
-Bf(.101535, .223656, lower=-1, meanoftheory=.298566, sdtheory=.01996)
+# Valadez & Ferguson, interaction effect violent (RDR either version) vs nonviolent (FIFA)
+esciTest(.22, 100, .4) # MAY BE INVALIDATED BY UNEQUAL CELL SIZES?
 # Valdez & Ferguson, interaction effect as originally reported
-Bf(.101535, .171667, lower=-1, meanoftheory=.298566, sdtheory=.01996)
+esciTest(.17, 100, .4)
 # Przybylski et al., Study 1
-Bf(.102062, 0, lower=-1, meanoftheory=.298566, sdtheory=.01996)
+esciTest(.004, 100, .4)
 # Przybylski et al., Study 2
-Bf(.101015, .080171, lower=-1, meanoftheory=.298566, sdtheory=.01996)
-# Ivory & Kalyraman, 2007
-Bf(.09245, .13074, lower=-1, meanoftheory=.298566, sdtheory=.01996)
+esciTest(.08, 100, .4) # or is it -.08?
+# Ivory & Kalyaraman, 2007
+esciTest(.13, 120, .4)
 
 # Aggressive behavior
 # Elson et al. 2013 noise intensity
-Bf(.111111, .202733, lower=-1, meanoftheory=.213171, sdtheory=.026252)
+esciTest(.2, 84, .4)
 # Elson et al. 2013 noise duration
-Bf(.111111, .110447, lower=-1, meanoftheory=.213171, sdtheory=.026252)
+esciTest(.11, 84, .4)
 # Ferguson et al. 2008
-Bf(.145865, .020003, lower=-1, meanoftheory=.213171, sdtheory=.026252)
+esciTest(.02, 50, .4)
 # Ferguson & Rueda, Violent vs Nonviolent game
-Bf(.116248, .01, lower=-1, meanoftheory=.213171, sdtheory=.026252)
+esciTest(.01, 77, .4)
 # Adachi & Willoughby 2011 exp 1
-Bf(.160128, .0, lower=-1, meanoftheory=.213171, sdtheory=.026252)
+esciTest(0, 42, .4)
 # Adachi & Willoughby 2011 exp 2
-Bf(.132453, .030009, lower=-1, meanoftheory=.213171, sdtheory=.026252)
+esciTest(.03, 60, .4)
 
 # Aggressive cognition
 # Ivory & Kalyaraman, 2007
-Bf(.09245, -.08017, lower=-1, meanoftheory=.223656, sdtheory=.018621)
+esciTest(-.08, 120, .4)
+
+
+# List of BF02's from Dienes-Christie calculator
+BF02 = function(mean, sd, lower, meanoftheory, sdtheory) {
+  return(1/Bf(obtained=mean, sd=sd, lower=lower, meanoftheory=meanoftheory, sdtheory=sdtheory)$BayesFactor)
+}
+  
+# All effect sizes should be in r-to-Z units  
+# Aggressive affect
+# Valdez & Ferguson, interaction effect violent (RDR either version) vs nonviolent (FIFA)
+BF02(sd=.101535, .223656, lower=-1, meanoftheory=.298566, sdtheory=.01996)
+
+# Valdez & Ferguson, interaction effect as originally reported
+BF02(sd=.101535, .171667, lower=-1, meanoftheory=.298566, sdtheory=.01996)
+# Przybylski et al., Study 1
+BF02(sd=.102062, 0, lower=-1, meanoftheory=.298566, sdtheory=.01996)
+# Przybylski et al., Study 2
+BF02(sd=.101015, .080171, lower=-1, meanoftheory=.298566, sdtheory=.01996)
+# Ivory & Kalyraman, 2007
+BF02(sd=.09245, mean=.13074, lower=-1, meanoftheory=.298566, sdtheory=.01996)
+
+# Aggressive behavior
+# Elson et al. 2013 noise intensity
+BF02(sd=.111111, .202733, lower=-1, meanoftheory=.213171, sdtheory=.026252)
+# Elson et al. 2013 noise duration
+BF02(sd=.111111, .110447, lower=-1, meanoftheory=.213171, sdtheory=.026252)
+# Ferguson et al. 2008
+BF02(sd=.145865, .020003, lower=-1, meanoftheory=.213171, sdtheory=.026252)
+# Ferguson & Rueda, Violent vs Nonviolent game
+BF02(sd=.116248, .01, lower=-1, meanoftheory=.213171, sdtheory=.026252)
+# Adachi & Willoughby 2011 exp 1
+BF02(sd=.160128, .0, lower=-1, meanoftheory=.213171, sdtheory=.026252)
+# Adachi & Willoughby 2011 exp 2
+BF02(sd=.132453, .030009, lower=-1, meanoftheory=.213171, sdtheory=.026252)
+
+# Aggressive cognition
+# Ivory & Kalyaraman, 2007
+BF02(sd=.09245, -.08017, lower=-1, meanoftheory=.223656, sdtheory=.018621)
 
 
 ## BF01 / BF10 for study 3 of Elson's CRTT complaint
